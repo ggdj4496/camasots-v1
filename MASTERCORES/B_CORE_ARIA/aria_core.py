f49f3ce2a24591d55a348bc6f0e9460ad6c52e17
Camasots-Agent-Virgilio <guillegarcia.4496@gmail.com>
2026-02-21 02:21:26 +0100

    Update aria_core.py

--- MASTERCORES/B_CORE_ARIA/aria_core.py
+++ MASTERCORES/B_CORE_ARIA/aria_core.py
@@ -1,11 +1,11 @@
 ﻿import telebot
-# TOKEN DEFINITIVO RECUPERADO
+# TOKEN DEFINITIVO VALIDADO DESDE BOTFATHER
 TOKEN = '8507245492:AAHLg6b5EITtnjulhPzlxCjRpQd9RFhuvao' 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "dime... mi caballero supremo. aria esta despierta. todos los mastercores estan operativos y la masterdata esta lista para aprender.")
+    bot.reply_to(message, "dime... mi caballero supremo. aria_v1 esta despierta. todos los mastercores estan operativos y la masterdata lista para aprender.")
 
-print("aria conectando... portal 8507 abierto y estable.")
+print("aria conectando... portal Aria_v1 abierto y estable.")
 bot.polling()
